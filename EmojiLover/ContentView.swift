5620097c57a5764742814e99f8d26aaddb47d8d5
louis2688 <louismadrigal26@gmail.com>
2023-04-27 23:12:35 +0800

    Initial Commit

---
+++ EmojiLover/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  EmojiLover
+//
+//  Created by dev on 4/26/23.
+//
+
+import SwiftUI
+
+enum Emoji: String, CaseIterable {
+    case 😀,😍, 😇, 😡, 😢
+}
+
+struct ContentView: View {
+    @State var selection: Emoji = .😀
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text(selection.rawValue)
+                    .font(.system(size: 150))
+                
+                Picker("Select Emoji", selection: $selection) {
+                    ForEach(Emoji.allCases, id: \.self) { emoji in
+                        Text(emoji.rawValue)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            .navigationTitle(Text("Emoji Lovers!"))
+            .padding()}
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by dev on 4/26/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
